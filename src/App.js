7d41db9a4471153db7e3f8381c911008e4bd7902
Mohamed Ahmed <ma614351@gmail.com>
2023-06-21 16:42:59 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import react from 'react'
-import Person from './components/Person'
+import { useState ,useEffect } from 'react';
+//import Person from './components/Person'
 import "./App.css";
 
 
-function App() {
+/* function App() {
   const name = "john";
   const isShowing = true;
 
   return (
     <div className="App">
       <h1>hello {!isShowing ? name : 'someone'}</h1>
-      <Person name ='mohamed' />
-      <Person/>
-    </div>
+      <Person name ='mohamed' lastname = "ahmed" age = "24" />
+     </div>
   );
 }
-
+ */
+function App() {
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    alert("hello world")
+  } )
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount -1 ) }>-</button>
+      <h1>{ counter}</h1>
+      <button onClick={() => setCounter((prevCount)=> prevCount +1 ) } >+</button>
+    </div>
+  )
+}
 export default App;
--- src/components/Person.js
+++ src/components/Person.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Person(props) {
   return (
     <div>
-      <p>{props.name} </p>
-      <p>last name : smith</p>
-      <p>age : 30</p>
+      <p> name : {props.name} </p>
+          <p> lastName :  { props.lastname}</p>
+          <p> age : { props.age}</p>
     </div>
   );
 }
